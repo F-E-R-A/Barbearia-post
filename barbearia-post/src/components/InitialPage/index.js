e60699d488145af26ace8a0c5fa98eee90dc7b43
Marcos André <88692791+F-E-R-A@users.noreply.github.com>
2023-02-23 13:13:03 -0200

    Update 2.0

--- barbearia-post/src/components/InitialPage/index.js
+++ barbearia-post/src/components/InitialPage/index.js
@@ -1,42 +1,20 @@
 import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
 import './style.css';
 
+import Header from '../Header';
+import Footer from '../Footer';
+
 export default function InitialPage(){
-    const Light = 'initialpageLight';
-    const Dark = 'initialpageDark';
-
-    const [ tema, setTema ] = useState(Light);
-
-    function mudarTema(){
-        if(tema === 'initialpageLight'){
-            setTema(Dark);
-        }else{
-            setTema(Light);
-        }
-    }
-
+    
+ 
     return(
-        <div className={tema}>
-            <header className="header">
-                <div className="div-logo">
-                    <img src="/assets/barbearia-logo.png" alt="Logo-Empresa" title="Logo da Empresa"/>
-                </div>
-                <div className="nav-menu">
-                    <div className="button-dark">
-                        <button onClick={mudarTema}>Dark</button>
-                    </div>
-                    <div className="nav">
-                        <Link className="links">Sobre</Link>
-                        <Link className="links" to="">Modelos</Link>
-                        <Link className="links" to="">Galeria</Link>
-                    </div>
-                </div>
+        <div className="ini">
+            <header>
+                <Header/>
             </header>
             <main className="main">
                 <div className="capa">
                     <h1>BARBER &nbsp;SHOP</h1>
-                    
                 </div>
                 <div className="descr">
                     <h2>Bem-Vindo a Barber-Shop</h2>
@@ -50,9 +28,9 @@ export default function InitialPage(){
                     </p>
                 </div>
             </main>
-            <footer className="footer">
-                <h4>S. Kelly</h4>
+            <footer>
+                <Footer/>
             </footer>
         </div>
     );
-}
+};
