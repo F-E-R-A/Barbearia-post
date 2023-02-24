5f217b60a18a1982de30cd50868105b1c584d026
Marcos André <88692791+F-E-R-A@users.noreply.github.com>
2023-02-23 23:05:37 -0300

    Add files via upload

--- barbearia-post/src/components/InitialPage/index.js
+++ barbearia-post/src/components/InitialPage/index.js
@@ -1,16 +1,64 @@
-import React, {useState} from 'react';
+import React, {useState} from "react";
+import {Link} from 'react-router-dom';
 import './style.css';
 
-import Header from '../Header';
-import Footer from '../Footer';
-
 export default function InitialPage(){
+    const headerLight = 'headerLight';
+    const headerDark = 'headerDark';
+
+    const letterLight = 'linksLight';
+    const letterDark = 'linksDark';
     
+    const button_text_light = 'button-light';
+    const button_text_dark = 'button-dark';
+
+    const button_descr_dark = 'Dark';
+    const button_descr_light = 'Light';
+
+    const main_dark = 'initialpageDark';
+    const main_light = 'initialpageLight';
+
+    const [ tema, setTema ] = useState(headerLight);
+    const [ letter, setLetter ] = useState(letterDark);
+    const [ button_text_color, setButtonTextColor ] = useState(button_text_dark);
+    const [ button_text_descr, setButonTextDescr] = useState(button_descr_dark);
+    const [ main_theme, setMainTheme ] = useState(main_light);
+    
+
+    function mudarTema(){
+        if(tema === headerLight){
+            setTema(headerDark);
+            setLetter(letterLight);
+            setButtonTextColor(button_text_light);
+            setButonTextDescr(button_descr_light);
+            setMainTheme(main_dark);
+            
+        }else{
+            setTema(headerLight);
+            setLetter(letterDark);
+            setButtonTextColor(button_text_dark);
+            setButonTextDescr(button_descr_dark);
+            setMainTheme(main_light);
+          
+        }
+    }
  
     return(
-        <div className="ini">
-            <header>
-                <Header/>
+        <div className={main_theme}>
+            <header className={tema}>
+                <div className="div-logo">
+                    <img src="/assets/barbearia-logo.png" alt="Logo-Empresa" title="Logo da Empresa"/>
+                </div>
+                <div className="nav-menu">
+                    <div className="button-area">
+                        <button id="button-dark-mode" className={button_text_color} onClick={mudarTema}>{button_text_descr}</button>
+                    </div>
+                    <div className="nav">
+                        <Link id="link1" className={letter}>Sobre</Link>
+                        <Link id="link2" className={letter} to="">Modelos</Link>
+                        <Link id="link3" className={letter} to="">Galeria</Link>
+                    </div>
+                </div>
             </header>
             <main className="main">
                 <div className="capa">
@@ -28,9 +76,9 @@ export default function InitialPage(){
                     </p>
                 </div>
             </main>
-            <footer>
-                <Footer/>
-            </footer>
+            <footer className="footer">
+                <h4>S. Kelly</h4>
+            </footer>   
         </div>
     );
 };
